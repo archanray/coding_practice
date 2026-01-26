0c2ad61581d2ad35ec1da7a24d4fdbdff5f068c9
unknown <talk2archan@gmail.com>
2026-01-25 21:13:47 -0500

    incomplete group anagram

---
+++ neetcode150/group_anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        """
+        Solutions --
+        1) Sort each string and then check for match. O(m*n log n)
+        2) Hashtable
+        """
+        
+        for s in strs:
+            
+        return None
+
+q = Solution()
+print(q.groupAnagrams(strs = ["act","pots","tops","cat","stop","hat"]))
